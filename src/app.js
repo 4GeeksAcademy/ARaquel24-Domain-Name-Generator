4b2703972a9429ae68d03070f940fd3e5dd1a638
ARaquel24 <anarklprzrjs@gmail.com>
2024-08-03 03:25:00 +0000

    finish generador

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function generadorNombres(pronouns, adjectives, nouns) {
+    let domains = [];
+    for (let i = 0; i < pronouns.length; i++) {
+      for (let j = 0; j < adjectives.length; j++) {
+        for (let k = 0; k < nouns.length; k++) {
+          let domain = pronouns[i] + adjectives[j] + nouns[k] + ".com";
+          domains.push(domain);
+        }
+      }
+    }
+    return domains;
+  }
+  let dominiosNombre = generadorNombres(pronoun, adj, noun);
+  for (let l = 0; l < dominiosNombre.length; l++) {
+    console.log(dominiosNombre[l]);
+  }
+  let domainListText = dominiosNombre.join("\n");
+  document.getElementById("generar").innerText = domainListText;
 };
